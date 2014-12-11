c8c5e68de74153f255d5b82b23e873b4f0632c14
francoisdanieldugard <fduga002@fiu.edu>
2014-12-11 17:02:48 -0500

    Final

--- Code/ipop-14.07.0_ubuntu12/kill.sh
+++ Code/ipop-14.07.0_ubuntu12/kill.sh
@@ -1,8 +1,20 @@
+
 #!/bin/bash
 
-ps -fa | grep ipop
-sudo pkill ipop-tincan-x86_64
-ps -fa | grep python
-sudo pkill svpn_controller.py
-sudo pkill gvpn_controller.py
-sudo pkill mccvpn_controller.py
+VAR1=$(pgrep ipop)
+VAR2=$(pgrep python)
+
+#echo $VAR1
+#echo $VAR2
+ps aux | grep ipop
+if [ -n "$VAR1" ]; then
+   sudo kill $VAR1
+fi
+ps aux | grep python
+if [ -n "$VAR2" ]; then
+   sudo kill $VAR2
+fi
+echo
+ps aux | grep ipop
+ps aux | grep python
+
