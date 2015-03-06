8bbb7435ff799bc9eea79a93668c18a6640ee491
Francois D'Ugard <fduga002@fiu.edu>
2015-03-06 15:58:32 -0500

    Fixes self.peers_ip6/4 bug

--- Code/ipop-14.07.0_ubuntu12/mcvpn_controller.py
+++ Code/ipop-14.07.0_ubuntu12/mcvpn_controller.py
@@ -690,7 +690,8 @@ class MC2Server(UdpServer):
 
         logging.debug ("Self.Peers = %s", self.peers )
         logging.debug ( "Self.Peerslist = %s", self.peerlist )
-
+        logging.debug ("Self.peers_ip4 = %s", self.peers_ip4 )
+         logging.debug ("Self.peers_ip6 = %s", self.peers_ip6 )
 
         if hop_count == 0:
             # make hop final destination
