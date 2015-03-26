68a060b8b9e4a2fc618b42cd0fea28ac51974932
Francois D'Ugard <fduga002@fiu.edu>
2015-03-26 13:51:43 -0400

    Adds new ipop-tincan

--- Code/ipop-14.07.0_ubuntu12/mcvpn_controller.py
+++ Code/ipop-14.07.0_ubuntu12/mcvpn_controller.py
@@ -203,7 +203,7 @@ class MCCVPNUdpServer(UdpServer):
         for f in range(0, CONFIG['mcc_forwards']):
             if(len(self.peers) > 0):
                 res = random.sample(self.peers, 1)
-                logging.debug(res)
+                logging.debug(res.value)
                 rand_dest = self.peers[res]
             else:
                 logging.debug("Sample larger than population")
